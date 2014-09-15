01e1afe60767d906d7d28ba8518e4977c0955938
Toby Goodwin <toby@paccrat.org>
2014-09-15 22:18:16 +0100

    more test suites

--- test/tester.c
+++ test/tester.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
-int main(void) {
-    return 0;
+#include <mref.h>
+
+int main(int argc, char **argv) {
+    if (argc != 2) return 99;
+    if (mref_valid(argv[1]))
+        return 0;
+    else
+        return 1;
 }
