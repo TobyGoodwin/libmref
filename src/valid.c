a8873ea1f1c0f9b4c180c525101ab39a2bdf9dbd
Toby Goodwin <toby@paccrat.org>
2014-09-16 20:41:02 +0100

    nicer test suite

--- src/valid.c
+++ src/valid.c
@@ -1,5 +1,18 @@
 #include "mref.h"
 
 int mref_valid(mref m) {
-	return 1;
+    if (!m) return 0;
+
+    /* version number */
+    if (*m != '0') return 0;
+    ++m;
+
+    /* flags */
+    while (*m != '\0' && *m != ',') {
+        if (*m != 'L') return 0;
+        ++m;
+    }
+    if (*m != ',') return 0;
+
+    return 1;
 }
