4a3d3c2b8a614ed2961242b4dddba9940b7fff78
Toby Goodwin <toby@paccrat.org>
2014-09-16 22:46:17 +0100

    mref_valid() inching along

--- src/valid.c
+++ src/valid.c
@@ -1,18 +1,34 @@
 #include "mref.h"
 
 int mref_valid(mref m) {
+    int c, n, comma[5];
     if (!m) return 0;
 
+    /* find all 5 commas */
+    c = 0;
+    for (n = 0; m[n] != '\0'; ++n)
+        if (m[n] == ',') {
+            if (c == 5) return 0;
+            comma[c++] = n;
+        }
+    if (c != 5) return 0;
+
     /* version number */
-    if (*m != '0') return 0;
-    ++m;
+    n = 0;
+    if (m[n] != '0') return 0;
 
     /* flags */
-    while (*m != '\0' && *m != ',') {
+    for (++n; n < comma[0]; ++n)
         if (*m != 'L') return 0;
-        ++m;
-    }
-    if (*m != ',') return 0;
+
+    /* sender - not much to say? */
+    for (++n; n < comma[1]; ++n)
+        ;
+
+    /* recipient */
+    /* not empty */
+    if (comma[2] - comma[1] == 1)
+        return 0;
 
     return 1;
 }
