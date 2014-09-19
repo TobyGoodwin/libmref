938192c284e65a8ddeb97d8692e39dbaa98e1d9b
Toby Goodwin <toby@paccrat.org>
2014-09-19 21:10:33 +0100

    now checking mref hashes

--- src/parse.c
+++ src/parse.c
@@ -1,3 +1,6 @@
+#include <gcrypt.h>
+#include <stdio.h>
+
 #include "mref.h"
 
 int mref_parse(mref m, int *field) {
@@ -37,6 +40,34 @@ int mref_parse(mref m, int *field) {
     p = field[4]; q = field[5] - 1;
     /* not empty */
     if (q - p != 44) return 7;
+
+    {
+        gcry_error_t gerr;
+        gcry_md_hd_t ghd;
+        unsigned char h[32], h64[45];
+        int i, j;
+        unsigned char map[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+        gcry_check_version(0);
+        gerr = gcry_md_open(&ghd, GCRY_MD_SHA256, 0);
+        if (!ghd) return 8;
+        gcry_md_write(ghd, m, field[4] - 1);
+        memcpy(h, gcry_md_read(ghd, 0), 32);
+        gcry_md_close(ghd);
+        for (i = 0, j = 0; i < 30; i += 3) {
+            h64[j++] = map[ h[i] >> 2 ];
+            h64[j++] = map[ (h[i] & 3) << 4 | h[i+1] >> 4 ];
+            h64[j++] = map[ (h[i+1] & 0xf) << 2 | h[i+2] >> 6 ];
+            h64[j++] = map[ h[i+2] & 0x3f ];
+        }
+        h64[j++] = map[ h[i] >> 2 ];
+        h64[j++] = map[ (h[i] & 3) << 4 | h[i+1] >> 4 ];
+        h64[j++] = map[ (h[i+1] & 0xf) << 2 ];
+        h64[j++] = '=';
+        h64[j++] = '\0';
+        printf("hash is %s\n", h64);
+        if (memcmp(m + p, h64, 44) != 0) return 9;
+    }
 
     return 0;
 }
